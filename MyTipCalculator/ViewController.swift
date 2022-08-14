463d70c3e16fa9577da64d27e3262df536bdcccf
dolnuea <dolunaydagci@outlook.com>
2022-08-14 13:59:17 -0400

    Initial Commit

---
+++ MyTipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTipCalculator
+//
+//  Created by dolu on 8/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
